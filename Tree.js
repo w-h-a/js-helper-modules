a5db343517da46a9684c68bb6bc23001e057a7e9
Wes Anderson <wesley.herb.anderson@gmail.com>
2021-09-28 15:58:13 -0400

    updating Tree

--- Tree.js
+++ Tree.js
@@ -1,6 +1,3 @@
-"use strict";
-
-
 class Branch {
     constructor (value, left, right) {
         this.value = value;
@@ -13,51 +10,132 @@ class Branch {
 class Leaf { }
 
 
-function size (tree) {
-    return tree instanceof Leaf ? 0
-                                : 1 + size (tree.left) + size (tree.right);
-}
+// non-exposed helpers
+const rightOf =
+    transient =>
+        transient.left instanceof Branch
+            ? new Branch ( transient.value
+                         , rightOf (transient.left)
+                         , transient.right
+                         )
+            : transient.right;
 
 
-function depth (tree) {
-    return tree instanceof Leaf ? 0
-                                : 1 + Math.max ( dpeth (tree.left), depth (tree.right) );
-}
+const lift =
+    transient =>
+        transient.left instanceof Branch
+            ? lift (transient.left)
+            : transient.value;
 
 
-function reflect (tree) {
-    return tree instanceof Leaf ? new Leaf ()
-                                : new Branch ( tree.value, reflect (tree.right), reflect (tree.left) );
-}
+// exposed
+const size =
+    tree =>
+        tree instanceof Leaf
+            ? 0
+            : 1 + size (tree.left) + size (tree.right);
 
 
-function inorder (tree, values = []) {
-    return tree instanceof Leaf ? values
-                                : inorder ( tree.left, [ tree.value, ...inorder (tree.right, values) ] );
-}
+const depth =
+    tree =>
+        tree instanceof Leaf
+            ? 0
+            : 1 + Math.max (depth (tree.left), depth (tree.right));
 
 
-function balanced (list) {
-    if (list.length === 0) return new Leaf ();
-    const k = Math.floor (list.length / 2);
-    const [ y, ...ys ] = list.slice (k);
-    return new Branch (y, balanced (list.slice (0, k)), balanced (ys));
-}
+const reflect =
+    tree =>
+        tree instanceof Leaf
+            ? new Leaf ()
+            : new Branch (tree.value, reflect (tree.right), reflect (tree.left));
 
 
-function completeTree (k, n) {
-    return n === 0 ? new Leaf ()
-                   : new Branch (k, completeTree (2 * k, n - 1), completeTree (2 * k + 1, n - 1));
-}
+const inorder =
+    (tree, values = []) =>
+        tree instanceof Leaf
+            ? values
+            : inorder (tree.left, [ tree.value, ...inorder (tree.right, values) ]);
 
 
-Object.assign (module.export, {
+const balanced =
+    arr =>
+        arr.length === 0
+            ? new Leaf ()
+            : (k =>
+                  (([ y, ...ys ]) =>
+                      new Branch (y, balanced (arr.slice (0, k)), balanced (ys))
+                  ) (arr.slice (k))
+              ) (Math.floor (arr.length / 2));
+
+
+const completeTree =
+    (k, n) =>
+        n === 0
+            ? new Leaf ()
+            : new Branch ( k
+                         , completeTree (2 * k, n - 1)
+                         , completeTree (2 * k + 1, n - 1)
+                         );
+
+
+const lookup =
+    (tree, value) =>
+        tree instanceof Leaf
+            ? null
+            : tree.value > value
+                ? lookup (tree.left, value)
+                : tree.value < value
+                    ? lookup (tree.right, value)
+                    : tree;
+
+
+const insert =
+    (tree, value) =>
+        tree instanceof Leaf
+            ? new Branch (value, new Leaf (), new Leaf ())
+            : tree.value > value
+                ? new Branch (tree.value, insert (tree.left, value), tree.right)
+                : tree.value < value
+                    ? new Branch (tree.value, tree.left, insert (tree.right, value))
+                    : new Error ("entry already exists");
+
+
+const destroy =
+    (tree, value) =>
+        tree instanceof Leaf
+            ? new Leaf ()
+            : tree.value > value
+                ? new Branch (tree.value, destroy (tree.left, value), tree.right)
+                : tree.value < value
+                    ? new Branch (tree.value, tree.left, destroy (tree.right, value))
+                    : tree.left instanceof Leaf
+                        ? tree.right
+                        : tree.right instanceof Leaf
+                            ? tree.left
+                            : new Branch ( lift (tree.right)
+                                         , tree.left
+                                         , rightOf (tree.right)
+                                         ); // not proud of this
+
+
+const max =
+    tree =>
+        tree.right instanceof Branch
+            ? max (tree.right)
+            : tree.value;
+
+
+Object.assign (module.exports, {
     Branch,
     Leaf,
     balanced,
     completeTree,
     depth,
+    destroy, // delete
     inorder,
+    insert, // insert
+    lookup, // search
+    max,
     reflect,
     size,
 });
